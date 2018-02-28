9072514de258e1943da2e6ee16641b499266a678
Duncan <duncan.mackinnon@vortexa.com>
2018-02-28 17:09:39 +0000

    Basic map style switching example

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,113 @@
-import * as React from 'react';
-import './App.css';
+import "./App.css";
 
-const logo = require('./logo.svg');
+import * as React from "react";
 
-class App extends React.Component {
-  render() {
+import {
+  DarkMapStyle,
+  LightMapStyle,
+  MapStyle,
+  styleConfig
+} from "./mapStyles";
+import ReactMapboxGl, { GeoJSONLayer } from "react-mapbox-gl";
+
+import styled from "styled-components";
+
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2o0MHp2cGtiMGFrajMycG5nbzBuY2pjaiJ9.QDApU0XH2v35viSwQuln5w"
+});
+
+const Container = styled.div`
+  position: relative;
+  height: 100%;
+  flex: 1;
+`;
+
+const Button = styled.button`
+  border: 1px solid #3770c6;
+  background-color: rgb(84, 152, 255);
+  height: 100%;
+  color: white;
+  font-size: 13px;
+  padding: 6px 12px;
+  border-radius: 6px;
+  cursor: pointer;
+  outline: none;
+  :hover {
+    background-color: #3770c6;
+  }
+`;
+const Indicator = styled.div`
+  padding: 6px 10px;
+  background-color: white;
+`;
+const BottomBar = styled.div`
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  right: 20px;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const mapStyle = {
+  height: "100%",
+  width: "100%"
+};
+
+export interface State {
+  styleKey: MapStyle;
+}
+
+class StyleUpdate extends React.Component<{}, State> {
+  public state: State = {
+    styleKey: LightMapStyle
+  };
+
+  public nextStyle = () => {
+    const { styleKey } = this.state;
+
+    this.setState({
+      styleKey: styleKey === LightMapStyle ? DarkMapStyle : LightMapStyle
+    });
+  };
+
+  public render() {
+    const { styleKey } = this.state;
+
+    const styleObj = styleConfig[styleKey];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Container>
+          <Map style={styleObj.url} containerStyle={mapStyle}>
+            <GeoJSONLayer
+              data={styleObj.geoJson}
+              circleLayout={{ visibility: "visible" }}
+              circlePaint={{
+                "circle-color": {
+                  property: "color",
+                  type: "identity"
+                }
+              }}
+              symbolLayout={{
+                "text-field": "{place}",
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 0.6],
+                "text-anchor": "top"
+              }}
+            />
+          </Map>
+          <BottomBar>
+            <Button onClick={this.nextStyle}>Change style</Button>
+            <Indicator>{`Using style: ${styleKey}`}</Indicator>
+          </BottomBar>
+        </Container>
       </div>
     );
   }
 }
 
-export default App;
+export default StyleUpdate;
---
+++ src/mapStyles.ts
@@ -0,0 +1,407 @@
+import { FeatureCollection, Point } from "geojson";
+
+const lightGeoJson: FeatureCollection<
+  Point,
+  { place: string; login: string; lat: string; lon: string; color: string }
+> = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        place: "The coffee bar",
+        login: "espresso",
+        lat: "38.91427",
+        lon: "-77.02827",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02827, 38.91427]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Bistro Bohem",
+        login: "2027355895",
+        lat: "38.91538",
+        lon: "-77.02013",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02013, 38.91538]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Black Cat",
+        login: "luckycat",
+        lat: "38.91458",
+        lon: "-77.03155",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.03155, 38.91458]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Snap",
+        login: "nutella1",
+        lat: "38.92239",
+        lon: "-77.04227",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.04227, 38.92239]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Columbia Heights Coffee",
+        login: "FAIRTRADE1",
+        lat: "38.93222",
+        lon: "-77.02854",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02854, 38.93222]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Azi's Cafe",
+        login: "sunny",
+        lat: "38.90842",
+        lon: "-77.02419",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02419, 38.90842]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Blind Dog Cafe",
+        login: "baxtercantsee",
+        lat: "38.91931",
+        lon: "-77.02518",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02518, 38.91931]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Le Caprice",
+        login: "baguette",
+        lat: "38.93260",
+        lon: "-77.03304",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.03304, 38.9326]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Filter",
+        login: "",
+        lat: "38.91368",
+        lon: "-77.04509",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.04509, 38.91368]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Peregrine",
+        login: "espresso",
+        lat: "38.88516",
+        lon: "-76.99656",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-76.99656, 38.88516]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Tryst",
+        login: "coupetnt",
+        lat: "38.921894",
+        lon: "-77.042438",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.042438, 38.921894]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "The Coupe",
+        login: "voteforus",
+        lat: "38.93206",
+        lon: "-77.02821",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02821, 38.93206]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Big Bear Cafe",
+        login: "",
+        lat: "38.91275",
+        lon: "-77.01239",
+        color: "#00FFFF"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.01239, 38.91275]
+      }
+    }
+  ]
+};
+
+const darkGeoJson: FeatureCollection<
+  Point,
+  { place: string; login: string; lat: string; lon: string; color: string }
+> = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        place: "The coffee bar",
+        login: "espresso",
+        lat: "38.91427",
+        lon: "-77.02827",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02827, 38.91427]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Bistro Bohem",
+        login: "2027355895",
+        lat: "38.91538",
+        lon: "-77.02013",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02013, 38.91538]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Black Cat",
+        login: "luckycat",
+        lat: "38.91458",
+        lon: "-77.03155",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.03155, 38.91458]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Snap",
+        login: "nutella1",
+        lat: "38.92239",
+        lon: "-77.04227",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.04227, 38.92239]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Columbia Heights Coffee",
+        login: "FAIRTRADE1",
+        lat: "38.93222",
+        lon: "-77.02854",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02854, 38.93222]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Azi's Cafe",
+        login: "sunny",
+        lat: "38.90842",
+        lon: "-77.02419",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02419, 38.90842]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Blind Dog Cafe",
+        login: "baxtercantsee",
+        lat: "38.91931",
+        lon: "-77.02518",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02518, 38.91931]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Le Caprice",
+        login: "baguette",
+        lat: "38.93260",
+        lon: "-77.03304",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.03304, 38.9326]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Filter",
+        login: "",
+        lat: "38.91368",
+        lon: "-77.04509",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.04509, 38.91368]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Peregrine",
+        login: "espresso",
+        lat: "38.88516",
+        lon: "-76.99656",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-76.99656, 38.88516]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Tryst",
+        login: "coupetnt",
+        lat: "38.921894",
+        lon: "-77.042438",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.042438, 38.921894]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "The Coupe",
+        login: "voteforus",
+        lat: "38.93206",
+        lon: "-77.02821",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.02821, 38.93206]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        place: "Big Bear Cafe",
+        login: "",
+        lat: "38.91275",
+        lon: "-77.01239",
+        color: "#000000"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [-77.01239, 38.91275]
+      }
+    }
+  ]
+};
+
+export const LightMapStyle = "light";
+export const DarkMapStyle = "dark";
+
+export type MapStyle = typeof LightMapStyle | typeof DarkMapStyle;
+
+export const styleConfig: {
+  [K in MapStyle]: {
+    url: string;
+    geoJson: FeatureCollection<
+      Point,
+      { place: string; login: string; lat: string; lon: string; color: string }
+    >;
+  }
+} = {
+  [LightMapStyle]: {
+    url: "mapbox://styles/mapbox/light-v9",
+    geoJson: lightGeoJson
+  },
+  [DarkMapStyle]: {
+    url: "mapbox://styles/mapbox/dark-v9",
+    geoJson: darkGeoJson
+  }
+};
